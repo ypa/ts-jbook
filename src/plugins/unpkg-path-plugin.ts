c2bce39077839b806f894b8a63de31587a8e23c8
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-28 20:13:43 -0800

    Refactor plugin to accept input instead of the hard-coded contents

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localForage.createInstance({
   name: 'filecache'
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -35,10 +35,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, { useState } from 'react-select';
-              console.log(React, useState, ReactDOM);
-            `,
+            contents: inputCode,
           };
         }
 
