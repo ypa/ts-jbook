e8251f7ccc0fd773b044cbfa59e324057db471d7
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-17 19:42:19 -0700

    Fix bug where editor closes up when clicked inside text area

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
       setEditing(false);
     };
     document.addEventListener('click', listener, { capture: true });
@@ -18,7 +26,7 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div>
+      <div ref={ref}>
         <MDEditor />
       </div>
     );
