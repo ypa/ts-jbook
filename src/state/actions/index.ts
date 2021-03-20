5d31b808efaf5d79a645313561baf0295c69cac5
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-19 22:48:21 -0700

    Add action interfaces

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,1 +1,37 @@
-export { }
+import { DeclareExportDeclaration } from 'jscodeshift';
+import { ActionType } from '../action-types';
+
+interface MoveCellAction {
+  type: ActionType.MOVE_CELL;
+  payload: {
+    id: string;
+    direction: 'up' | 'down';
+  }
+}
+
+interface DeleteCellAction {
+  type: ActionType.DELETE_CELL;
+  payload: string;
+}
+
+interface InsertCellBeforeAction {
+  type: ActionType.INSERT_CELL_BEFORE;
+  payload: {
+    id: string;
+    type: 'code' | 'text';
+  }
+}
+
+interface UpdateCellAction {
+  type: ActionType.UPDATE_CELL;
+  payload: {
+    id: string;
+    content: string;
+  }
+}
+
+export type Action =
+  MoveCellAction
+  | DeclareExportDeclaration
+  | InsertCellBeforeAction
+  | UpdateCellAction;
