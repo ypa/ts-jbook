f06bf65e7ba98ec311b573632b20a1ebe3efe39d
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-20 20:22:11 -0700

    Add boilerplate code for cellsReducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,1 +1,27 @@
-export { }
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell';
+
+interface CellsState {
+  loading: boolean;
+  error: string | null;
+  order: string[];
+  data: {
+    [key: string]: Cell
+  }
+}
+
+
+const initialState: CellsState = {
+  loading: false,
+  error: null,
+  order: [],
+  data: {}
+};
+
+
+const reducer = (state: CellsState = initialState, action: Action): CellsState => {
+  return state;
+};
+
+export default reducer;
