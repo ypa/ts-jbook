66dbfa593a3744f93e1b53e91af3e9a395ce9272
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-21 20:04:18 -0700

    Add action creators with mininal biolerplate

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,1 +1,10 @@
-export { }
+import { ActionType } from '../action-types';
+import { Action, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellBeforeAction } from '../actions';
+
+export const updateCell = (): UpdateCellAction => { };
+
+export const deleteCell = (): DeleteCellAction => { };
+
+export const moveCell = (): MoveCellAction => { };
+
+export const insertCellBefore = (): InsertCellBeforeAction => { };
