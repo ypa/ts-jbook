a3ad8c9dee35e8364ed17cba7ae394de2abb0f51
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-22 20:26:50 -0700

    Implement action creators

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,10 +1,47 @@
 import { ActionType } from '../action-types';
-import { Action, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellBeforeAction } from '../actions';
+import {
+  Action,
+  UpdateCellAction,
+  DeleteCellAction,
+  MoveCellAction,
+  InsertCellBeforeAction,
+  Direction
+} from '../actions';
+import { CellTypes } from '../cell';
 
-export const updateCell = (): UpdateCellAction => { };
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content
+    }
+  };
+};
 
-export const deleteCell = (): DeleteCellAction => { };
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id
+  };
+};
 
-export const moveCell = (): MoveCellAction => { };
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction
+    }
+  };
+};
 
-export const insertCellBefore = (): InsertCellBeforeAction => { };
+export const insertCellBefore = (id: string, cellType: CellTypes): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType
+    }
+  };
+};
