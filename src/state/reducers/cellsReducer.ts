3c67ef098815f01682b30efc9b302cd9fa558674
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-23 21:07:27 -0700

    Implement MOVE_CELL action reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -34,7 +34,17 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
 
       return;
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return;
     case ActionType.INSERT_CELL_BEFORE:
       return state;
     default:
