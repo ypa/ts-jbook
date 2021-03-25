3d913794e09d0ccffa9f7f55466928a2a87c15f3
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-24 18:16:27 -0700

    Explicitly return state to make typescript happy

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -21,30 +21,32 @@ const initialState: CellsState = {
 };
 
 
-const reducer = produce((state: CellsState = initialState, action: Action) => {
+const reducer = produce((state: CellsState = initialState, action: Action): CellsState => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload
 
       state.data[id].content = content;
-      return;
+
+      return state;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter(id => id !== action.payload);
 
-      return;
+      return state;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
-      return;
+
+      return state;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
         content: '',
