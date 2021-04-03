456591ea5565a450ee6e0844d9ca0b94bc795f4b
Yan P Aung <yan.pye.aung@gmail.com>
2021-04-02 19:41:20 -0700

    Add createBundle action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,3 +1,4 @@
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
 import {
   Action,
@@ -5,9 +6,12 @@ import {
   DeleteCellAction,
   MoveCellAction,
   InsertCellAfterAction,
+  BundleStartAction,
+  BundleCompleteAction,
   Direction
 } from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -45,3 +49,23 @@ export const insertCellAfter = (id: string | null, cellType: CellTypes): InsertC
     }
   };
 };
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId
+      }
+    });
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: result,
+      }
+    });
+  };
+}
