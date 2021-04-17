5de3e27534550ecb4d5009493000ed0a5f340ffa
Yan P Aung <yan.pye.aung@gmail.com>
2021-04-16 19:29:05 -0700

    Add better messaging

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -10,11 +10,15 @@ export const serveCommand = new Command()
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
       await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
     } catch (err) {
       if (err.code === 'EADDRINUSE') {
         console.error('Port is in use. Try running on a different port.');
       } else {
         console.log('Heres the problem', err.message);
       }
+      process.exit(1);
     }
   });
