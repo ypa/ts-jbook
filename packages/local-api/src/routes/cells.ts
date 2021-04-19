33eb9605e6182acc61c8633d0d132ad307bd2f44
Yan P Aung <yan.pye.aung@gmail.com>
2021-04-18 20:19:43 -0700

    Add route handler code to read and write cells to a file

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,24 +1,44 @@
 import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+  router.use(express.json());
+
+  const fullPath = path.join(dir, filename);
 
   router.get('/cells', async (req, res) => {
-    // Make sure the cell storage file exists
-    // If it does not exist, add in a default list of cells
+    try {
+      // Read the file
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
 
-    // Read the file
-    // Parse a list of cells out of it
-    // Send list of cells back to browser
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
   });
 
   router.post('/cells', async (req, res) => {
-    // Make sure the file exists
-    // If not, create it
-
     // Take the list of cells from the request obj
     // serialize them
+    const { cells }: { cells: Cell[] } = req.body;
+
     // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
   });
 
   return router;
