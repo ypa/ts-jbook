7b93b47c2babd17b37c60f47994abb78800754f3
Yan P Aung <yan.pye.aung@gmail.com>
2021-04-22 19:20:55 -0700

    Add debouncing logic for persisting cell state

--- packages/local-client/src/state/middlewares/persist-middleware.ts
+++ packages/local-client/src/state/middlewares/persist-middleware.ts
@@ -5,6 +5,8 @@ import { saveCells } from '../action-creators';
 import { RootState } from '../reducers';
 
 export const persistMiddleware = ({ dispatch, getState }: { dispatch: Dispatch<Action>; getState: () => RootState }) => {
+  let timer: any;
+
   return (next: (action: Action) => void) => {
     return (action: Action) => {
       next(action);
@@ -15,7 +17,12 @@ export const persistMiddleware = ({ dispatch, getState }: { dispatch: Dispatch<A
         ActionType.INSERT_CELL_AFTER,
         ActionType.DELETE_CELL].includes(action.type)
       ) {
-        saveCells()(dispatch, getState);
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          saveCells()(dispatch, getState);
+        }, 250);
       }
     };
   };
